06aceed43a1fda0f94f41d0f3b9b3a4dfe3abc44
Paxton <dracarys1312@gmail.com>
2020-12-06 19:11:10 +0700

    Add payload and controller

---
+++ backend/src/main/java/controllers/TestController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+	@GetMapping("/all")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/patient")
+	@PreAuthorize("hasRole('PATIENT') or hasRole('DOCTOR') or hasRole('ADMIN')")
+	public String userAccess() {
+		return "Patient Content.";
+	}
+
+	@GetMapping("/doctor")
+	@PreAuthorize("hasRole('DOCTOR')")
+	public String moderatorAccess() {
+		return "Doctor Board.";
+	}
+
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String adminAccess() {
+		return "Admin Board.";
+	}
+}
---
+++ backend/src/main/java/payload/request/SignupRequest.java
@@ -0,0 +1,102 @@
+package payload.request;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.validation.constraints.*;
+ 
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+ 
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    
+    private Set<String> role;
+    
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+  
+    @NotBlank
+	@Size(max = 20)
+	private String phone;
+	
+	@NotBlank
+	@Size(max = 500)
+	private String address;
+	
+	@NotBlank
+	@Size(max = 10)
+	private String sex;
+	
+	@Past
+	private Date dob;
+	
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAdress(String address) {
+		this.address = address;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+    public Set<String> getRole() {
+      return this.role;
+    }
+    
+    public void setRole(Set<String> role) {
+      this.role = role;
+    }
+}
