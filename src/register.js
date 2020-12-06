ca848ca38ddcd3d64e97f28fbfc70f15437de089
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-12-06 10:18:51 +0700

    Upload the register.js

--- src/register.js
+++ src/register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
@@ -17,10 +17,14 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import moment from 'moment';
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
 export default function Register(){
   const styles = useStyles();
   const [sex,setSex] = React.useState('');
+  const [dob, setDob] = useState(moment().format("DD/MM/YYYY"))
 
   const handleChange = (event) => {
     setSex(event.target.value);
@@ -37,8 +41,8 @@ export default function Register(){
             Sign up
           </Typography>
           <form className={styles.form} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+            <Grid container spacing={3}>
+              <Grid item xs={6} sm={5}>
                 <TextField 
                   name="firstName"
                   // variant="outlined"
@@ -48,7 +52,7 @@ export default function Register(){
                   label="First Name"
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={6} sm={5}>
                 <TextField 
                   name="lastName"
                   // variant="outlined"
@@ -58,16 +62,7 @@ export default function Register(){
                   label="Last Name"
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField 
-                  name="firstName"
-                  // variant="outlined"
-                  fullWidth
-                  id="firstName"
-                  label="DOB"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={2} sm={2}>
                 <FormControl fullWidth className={styles.formControl}>
                   <InputLabel>Sex</InputLabel>
                   <Select
@@ -84,18 +79,76 @@ export default function Register(){
                 </Select>
                 </FormControl>
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={8} sm={8}>
                 <TextField
                   // variant="outlined"
                   required
                   fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
+                  id="phone"
+                  label="Phone"
+                  name="phone"
+                  autoComplete="phone"
                 />
-              </Grid>
+                </Grid>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                 <Grid item xs={4} sm={4}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    id="date-picker-inline"
+                    label="DOB"
+                    value={dob}
+                    onChange={e => {setDob(e)}}
+                  />
+                </Grid>
+              </MuiPickersUtilsProvider>
+            <Grid item xs={4} sm={4}>
+              <TextField
+                fullWidth
+                id="Street"
+                label="Street"
+                />
             </Grid>
+            <Grid item xs={4} sm={4}>
+              <TextField
+                fullWidth
+                id="Street"
+                label="District"
+                />
+            </Grid>
+            <Grid item xs={4} sm={4}>
+              <TextField
+                fullWidth
+                id="city"
+                label="City"
+                />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+              />
+            </Grid>
+            </Grid>
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            // color='#81b3cb'
+            className={styles.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
           </form>
         </div>
       </Grid>
@@ -132,4 +185,8 @@ const useStyles=makeStyles((theme)=>({
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#81b3cb'
+  }
 }))
