bf5f26619281c58c008da727dd60f0fa7309fe8f
Paxton <dracarys1312@gmail.com>
2020-12-06 17:23:29 +0700

    Start backend

---
+++ backend/src/main/java/models/ERole.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum ERole {
+	ROLE_PATIENT,
+    ROLE_DOCTOR,
+    ROLE_ADMIN
+}
---
+++ backend/src/main/java/models/User.java
@@ -0,0 +1,140 @@
+package models;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Date;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(	name = "users", 
+		uniqueConstraints = { 
+			@UniqueConstraint(columnNames = "username"),
+			@UniqueConstraint(columnNames = "email") 
+		})
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(max = 20)
+	private String username;
+
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
+	
+	@NotBlank
+	@Size(max = 120)
+	private String password;
+
+	@NotBlank
+	@Size(max = 20)
+	private String phone;
+	
+	@NotBlank
+	@Size(max = 500)
+	private String address;
+	
+	@NotBlank
+	@Size(max = 10)
+	private String sex;
+	
+	@Past
+	private Date dob;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(	name = "user_roles", 
+				joinColumns = @JoinColumn(name = "user_id"), 
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	public User() {
+	}
+
+	public User(String username, String email, String password, String address, String sex, String phone, Date dob) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+		this.dob = dob;
+		this.phone = phone;
+		this.sex = sex;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAdress(String address) {
+		this.address = address;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+}
