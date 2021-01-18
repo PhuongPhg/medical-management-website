5b476ed3e089ff59c32c2bbb76e02913281c5e80
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2021-01-18 16:45:02 +0700

    Update ScheduleComponent

--- src/views/schedule.js
+++ src/views/schedule.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Navigation from '../navigation';
+import { ScheduleComponent,  Week, Month, Day, Inject, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
 
 const useStyles = makeStyles((theme) => ({
   container:{
@@ -30,7 +31,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Schedule(){
   const classes = useStyles();
-    
+  
+  const [data, setData] = useState(
+    [{
+      Id: 2,
+      Subject: 'Meeting',
+      StartTime: new Date(2018, 1, 15, 10, 0),
+      EndTime: new Date(2018, 1, 15, 12, 30),
+      IsAllDay: false,
+      Status: 'Completed',
+      Priority: 'High'
+  }],
+  )
   return(
     <div>
       <Navigation Schedule />
@@ -42,7 +54,15 @@ export default function Schedule(){
         </Grid>
         <Grid item xs={9}>
           <Paper className={classes.profile_contain} style={{width: '98%', marginLeft: 5}}>
-            pppp
+          <ScheduleComponent height='550px' selectedDate={new Date(2018, 1, 15)} eventSettings={{ dataSource: data,
+        }}>
+          <ViewsDirective>
+            <ViewDirective option='Day'/>
+            <ViewDirective option='Week'/>
+            <ViewDirective option='Month'/>
+          </ViewsDirective>
+          <Inject services={[Day, Week, Month]}/>
+        </ScheduleComponent>
           </Paper>
         </Grid>
       </Grid>
