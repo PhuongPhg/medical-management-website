1fde3d8794df7adc96d1e3159645dcb12043b8e0
PhuongPhg <thaophuong.trinh.9@gmail.com>
2021-01-31 00:21:49 +0700

    Add create appointment modal

--- src/views/schedule.js
+++ src/views/schedule.js
@@ -8,6 +8,21 @@ import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import { colors } from '../helpers/config.js';
 import { ScheduleComponent,  Week, Month, Day, Inject, ViewsDirective, ViewDirective } from '@syncfusion/ej2-react-schedule';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Avatar from '@material-ui/core/Avatar';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import moment from 'moment';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 export default function Schedule(){
   const classes = useStyles();
@@ -16,13 +31,25 @@ export default function Schedule(){
     [{
       Id: 2,
       Subject: 'Meeting',
-      StartTime: new Date(2021, 0, 19, 10, 0),
-      EndTime: new Date(2021, 0, 19, 12, 30),
+      StartTime: new Date(2021, 0, 29, 10, 0),
+      EndTime: new Date(2021, 0, 29, 12, 30),
       IsAllDay: false,
       Status: 'Completed',
       // Priority: 'High'
   }],
   )
+  const [name, setName] = useState(null);
+  const [id, setId] = useState(null);
+  const [symptom, setSymptom] = useState(null);
+  const [firstday, setFirstday] = useState(moment().format("DD/MM/YYYY"));
+  const [anamnesis, setAnamnesis] = useState(null);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const CreateEvent = () => {
       setData(
         [{
@@ -60,7 +87,7 @@ export default function Schedule(){
               </Grid>
               <Grid item xs={12} sm={6} style={{textAlign: 'right', }}>
                 <Tooltip title="Create an appointment">
-                  <IconButton aria-label="create" onClick={CreateEvent}>
+                  <IconButton aria-label="create" onClick={handleOpen}>
                     <AddIcon />
                   </IconButton>
                 </Tooltip>
@@ -93,7 +120,6 @@ export default function Schedule(){
           <Paper className={classes.profile_contain} style={{width: '98%', marginLeft: 5}}>
           <ScheduleComponent height='550px' selectedDate={new Date()} eventSettings={{ dataSource: data, allowAdding: false, allowDeleting: false, allowEditing: false}}
             actionComplete={()=> console.log(data)}
-            
             >
           <ViewsDirective>
             <ViewDirective option='Day'/>
@@ -105,6 +131,102 @@ export default function Schedule(){
           </Paper>
         </Grid>
       </Grid>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 300,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <ListAltIcon /> 
+          </Avatar>
+          <Typography variant="h5">
+            Medical Registration Form
+          </Typography>
+          <form className={classes.form} noValidate>
+            <Grid container>
+            <Grid item xs={12} sm={6}>
+              <TextField 
+                margin="normal"
+                required
+                fullWidth
+                variant="outlined"
+                id="patient-name"
+                label="Patient name"
+                onTextChange = {text => setName(text)}
+                style={{marginRight: '4px'}}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField 
+                margin="normal"
+                required
+                fullWidth
+                variant="outlined"
+                id="identification"
+                label="ID"
+                onTextChange = {text => setId(text)}
+                style={{marginLeft: '4px'}}
+              />
+            </Grid>
+            <TextField 
+              multiline
+              rows={4}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="symptom"
+              label="Describe your health problems"
+              onTextChange = {text => setSymptom(text)}
+            />
+            <TextField 
+              multiline
+              rows={4}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="anamnesis"
+              label="Anamnesis"
+              onTextChange={text => setAnamnesis(text)}
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker 
+                style={{marginTop: '16px'}}
+                disableToolbar
+                fullWidth
+                variant="inline"
+                inputVariant="outlined"
+                format="MM/dd/yyyy"
+                id="date-picker-inline"
+                label="Set appointment date"
+                value={firstday}
+                onChange={val => {setFirstday(val)}}
+              />
+            </MuiPickersUtilsProvider>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              className={classes.submit}
+              onSubmit={CreateEvent}
+            >
+              Submit
+            </Button>
+          </form>
+        </div>
+        </Fade>
+      </Modal>
     </div>
   )
 }
@@ -125,11 +247,13 @@ const useStyles = makeStyles((theme) => ({
     margin: 'auto',
   },
   paper: {
-    // paddingLeft: 20,
-    // backgroundColor: '#FAFAFA',
-    backgroundColor: '#555555',
-    minHeight: '85vh',
+    padding: 20,
+    backgroundColor: '#f6f5f5',
     alignContent: 'center',
+    textAlign: 'center',
+    maxWidth: '40%',
+    alignItems: 'center',
+    margin: 'auto'
   },
   bar:{
     // paddingTop: 10,
@@ -150,5 +274,20 @@ const useStyles = makeStyles((theme) => ({
     marginRight: 20,
     marginTop: 10,
     padding: 5,
-  }
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  avatar: {
+    margin: 'auto',
+    textAlign: 'center',
+    marginBottom: theme.spacing(1),
+    backgroundColor: colors.primary,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
 }));
