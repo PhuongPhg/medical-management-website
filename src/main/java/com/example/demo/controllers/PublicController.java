1bb4235d9739d0e78482fe975ee1377303b6beb1
provodoihi <profiloes@gmail.com>
2021-02-09 16:42:49 +0700

    Add update profile to public api(for convenient of front-end team)

--- src/main/java/com/example/demo/controllers/PublicController.java
+++ src/main/java/com/example/demo/controllers/PublicController.java
@@ -5,14 +5,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -32,6 +38,22 @@ public class PublicController {
 		Optional<User> userId = userRepository.findById(id);
 		if (userId.isPresent()) {
 			return new ResponseEntity<>(userId.get(),HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/user/{id}")
+	public ResponseEntity<User> updateUserById(@PathVariable("id") long id, @RequestBody User user){
+		Optional<User> userId = userRepository.findById(id);
+		if (userId.isPresent()) {
+			User userInfo = userId.get();
+			userInfo.setFirstname(user.getFirstname());
+			userInfo.setLastname(user.getLastname());
+			userInfo.setAdress(user.getAddress());
+			userInfo.setDob(user.getDob());
+			return new ResponseEntity<>(userRepository.save(userInfo),HttpStatus.OK);
 		}
 		else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
